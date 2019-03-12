a7a0174ead57efbf20dde626533ea9f325dcbb1e
DatDraggy <bmxstreet@gmx.net>
2019-03-12 21:46:05 +0100

    content length

--- viewer.php
+++ viewer.php
@@ -1,18 +1,26 @@
 <?php
-require_once (__DIR__.'/config/config.php');
+require_once(__DIR__ . '/config/config.php');
 
 if (!isset($_GET['file']) || empty($_GET['file'])) {
   die();
 }
 $file = $_GET['file'];
 
-$mimes = array('jpg'  => 'image/jpeg',
-               'png'  => 'image/png',
-               'gif'  => 'image/gif',
-               'webm' => 'video/webm'
+$mimes = array(
+  'jpg'  => 'image/jpeg',
+  'png'  => 'image/png',
+  'gif'  => 'image/gif',
+  'webm' => 'video/webm'
 );
 
 $ext = explode('.', $file)[1];
 
-header('Content-Type: '. $mimes[$ext]);
-echo file_get_contents("{$config['saveDir']}/$ext/$file");
+$filePath = "{$config['saveDir']}/$ext/$file";
+
+if (file_exists($filePath)) {
+  $filesize = filesize($filePath);
+
+  header('Content-Type: ' . $mimes[$ext]);
+  header('Content-Length: ' . $filesize);
+  echo file_get_contents($filePath);
+}
